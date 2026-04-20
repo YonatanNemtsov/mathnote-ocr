e4643fba8b4a56cd7411f83482d9e755bcab7490
Yonatan Nemtsov <73360515+YonatanNemtsov@users.noreply.github.com>
2026-04-20 17:59:09 +0300

    Print running URL on server startup regardless of log level

--- tools/web/server.py
+++ tools/web/server.py
@@ -370,4 +370,5 @@ if __name__ == "__main__":
     args = ap.parse_args()
 
     app = create_app(args.config)
+    print(f"Running on http://{args.host}:{args.port}")
     uvicorn.run(app, host=args.host, port=args.port, log_level="warning")
